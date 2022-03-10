8ffc8c41d943e795259cfebfea326efc20acf532
cpwill01 <williamming778@gmail.com>
2022-03-10 17:26:07 +0800

    Implement GUI support for Meetings

---
+++ src/main/java/seedu/address/ui/MeetingCard.java
@@ -0,0 +1,77 @@
+package seedu.address.ui;
+
+import java.util.Comparator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.meeting.Meeting;
+
+/**
+ * An UI component that displays information of a {@code Meeting}.
+ */
+public class MeetingCard extends UiPart<Region> {
+
+    private static final String FXML = "MeetingListCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Meeting meeting;
+
+    @FXML
+    private FlowPane participants;
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label date;
+    @FXML
+    private Label startToEndTime;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code PersonCode} with the given {@code Person} and index to display.
+     */
+    public MeetingCard(Meeting meeting, int displayedIndex) {
+        super(FXML);
+        this.meeting = meeting;
+        id.setText(displayedIndex + ". ");
+        name.setText(meeting.getName().value);
+        participants.getChildren().add(new Label(meeting.getParticipantsList().value));
+        date.setText(meeting.getDate().value);
+        startToEndTime.setText(meeting.getStartTime().value + " - " + meeting.getEndTime().value);
+        meeting.getTags().stream()
+                .sorted(Comparator.comparing(tag -> tag.tagName))
+                .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof MeetingCard)) {
+            return false;
+        }
+
+        // state check
+        MeetingCard card = (MeetingCard) other;
+        return id.getText().equals(card.id.getText())
+                && meeting.equals(card.meeting);
+    }
+}
