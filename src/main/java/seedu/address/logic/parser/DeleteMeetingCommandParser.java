4b87aaccc62820dcdbda3e1bb23ffa50b8b54548
YiFei2 <fongyifei1999@gmail.com>
2022-03-13 20:57:21 +0800

    Fix javadoc typos

--- src/main/java/seedu/address/logic/parser/DeleteMeetingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteMeetingCommandParser.java
@@ -7,13 +7,13 @@ import seedu.address.logic.commands.DeleteMeetingCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
- * Parses input arguments and creates a new DeleteCommand object
+ * Parses input arguments and creates a new DeleteMeetingCommand object
  */
 public class DeleteMeetingCommandParser implements Parser<DeleteMeetingCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the DeleteCommand
-     * and returns a DeleteCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteMeetingCommand
+     * and returns a DeleteMeetingCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteMeetingCommand parse(String args) throws ParseException {
